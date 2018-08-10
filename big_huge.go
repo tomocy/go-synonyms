cfaab7ec51456ef8755ec7f1be4c9166f4fe8f2d
tomocy <tomocy.dev@gmail.com>
2018-08-10 13:32:16 +0900

    fix

--- big_huge.go
+++ big_huge.go
@@ -1,9 +1,30 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type bigHuge struct {
 	clientKey string
 }
 
-func (h *bigHube) fetchSynonyms(term string) ([]string, error) {
+func (h bigHube) fetchSynonyms(term string) ([]string, error) {
 	syn := make([]string, 0)
+	resp, err := http.Get("http://word.bighugelabs.com/api/2/" + h.clientKey + "/" + term + "/json")
+	if err != nil {
+		return syn, fmt.Errorf("could not get the reponse from big huge: %s", err)
+	}
+	defer resp.Body.Close()
+
+	var decodedResp synonyms
+	if err := json.NewDecoder(resp.Body).Decode(&decodedResp); err != nil {
+		return syn, fmt.Errorf("could not decode response body: %s", err)
+	}
+
+	syn = append(syn, decodedResp.Norn.Syn...)
+	syn = append(syn, decodedResp.Verb.Syn...)
+
+	return syn, nil
 }
